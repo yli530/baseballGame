bb28fbf97d4d4e3722318d4e327d9b64bf10d98f
yli530 <yli530@ucsc.edu>
2021-10-14 18:03:53 -0700

    format + merge

--- game/main.js
+++ game/main.js
@@ -1,21 +1,17 @@
-<<<<<<< HEAD
-title = "Baseball Game";
-=======
 title = "game";
->>>>>>> 30639241c1224c6c510cb239078ad03d1d09f6bb
 
 description = `
 `;
 
 characters = [
-`
+  `
  lll
 lr rl 
 l r l
 lr rl
  lll
 `,
-`
+  `
   r
  lll
 lllll
@@ -70,25 +66,25 @@ const G = {
   BALL_MIN_BASE_SPEED: 1,
   BALL_MAX_BASE_SPEED: 1.5,
   WIDTH: 100,
-	HEIGHT: 150
+  HEIGHT: 150
 };
 
 function update() {
   if (!ticks) {
-<<<<<<< HEAD
     balls = [];
     waveCount = 0;
     ballSpeed = 0;
-    bombs = []
+    bombs = [];
+    startTime = Date.now();
   }
 
   if (balls.length === 0) {
     ballSpeed =
-        rnd(G.BALL_MIN_BASE_SPEED, G.BALL_MAX_BASE_SPEED) * difficulty;
+      rnd(G.BALL_MIN_BASE_SPEED, G.BALL_MAX_BASE_SPEED) * difficulty;
     for (let i = 0; i < 9; i++) {
-        const posX = rnd(0, G.WIDTH);
-        const posY = -rnd(i * G.HEIGHT * 0.1);
-        balls.push({ pos: vec(posX, posY) })
+      const posX = rnd(0, G.WIDTH);
+      const posY = -rnd(i * G.HEIGHT * 0.1);
+      balls.push({ pos: vec(posX, posY) })
     }
   }
   remove(balls, (ba) => {
@@ -96,30 +92,24 @@ function update() {
     char("a", ba.pos);
 
     return (ba.pos.y > G.HEIGHT);
-});
-console.log(ballSpeed);
+  });
+  console.log(ballSpeed);
 
-if (bombs.length === 0) {
-  ballSpeed =
-    rnd(G.BALL_MIN_BASE_SPEED, G.BALL_MAX_BASE_SPEED) * difficulty;
-  for (let i = 0; i < 3; i++) {
+  if (bombs.length === 0) {
+    ballSpeed =
+      rnd(G.BALL_MIN_BASE_SPEED, G.BALL_MAX_BASE_SPEED) * difficulty;
+    for (let i = 0; i < 3; i++) {
       const posX = rnd(0, G.WIDTH);
       const posY = -rnd(i * G.HEIGHT * 0.1);
       bombs.push({ pos: vec(posX, posY) })
+    }
   }
-}
-remove(bombs, (bo) => {
-  bo.pos.y += ballSpeed;
-  char("b", bo.pos);
+  remove(bombs, (bo) => {
+    bo.pos.y += ballSpeed;
+    char("b", bo.pos);
 
-  return (bo.pos.y > G.HEIGHT);
-});
-//text(balls.length.toString(), 3, 10);
-
-}
-=======
-    startTime = Date.now();
-  }
+    return (bo.pos.y > G.HEIGHT);
+  });
 
   color("yellow");
   if (input.isJustPressed) {
@@ -129,11 +119,12 @@ remove(bombs, (bo) => {
     line(50, 50, 66, 66, 5);
   }
 
+  color("light_black");
   text("Time Left:", 14, 10);
   num = Math.floor(61 - ((Date.now() - startTime) / 1000));
-  if(num < 0) {
+  if (num <= 0) {
     end();
   }
   text(num.toString(), 77, 10);
+
 }
->>>>>>> 30639241c1224c6c510cb239078ad03d1d09f6bb
