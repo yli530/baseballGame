618e7c933ff86b594807dc00e59981b5384719b7
justin990627 <justin990627@gmail.com>
2021-10-14 17:52:51 -0700

    add bat into the game

--- game/main.js
+++ game/main.js
@@ -17,6 +17,14 @@ function update() {
     startTime = Date.now();
   }
 
+  color("yellow");
+  if (input.isJustPressed) {
+    play("laser"); //change the sound later
+    line(50, 50, 66, 36, 5);
+  } else {
+    line(50, 50, 66, 66, 5);
+  }
+
   text("Time Left:", 14, 10);
   num = Math.floor(61 - ((Date.now() - startTime) / 1000));
   if(num == 0) {
