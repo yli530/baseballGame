4de83282877424379eaa4a5690910b1ec931c4c4
yli530 <yli530@ucsc.edu>
2021-10-14 17:41:09 -0700

    added timer

--- game/main.js
+++ game/main.js
@@ -1,9 +1,12 @@
-title = "";
+title = "game";
 
 description = `
 `;
 
 characters = [];
+
+let startTime;
+let num = 1;
 
 options = {
   theme: 'pixel'
@@ -11,5 +14,13 @@ options = {
 
 function update() {
   if (!ticks) {
+    startTime = Date.now();
   }
+
+  text("Time Left:", 14, 10);
+  num = Math.floor(61 - ((Date.now() - startTime) / 1000));
+  if(num == 0) {
+    end();
+  }
+  text(num.toString(), 77, 10);
 }
